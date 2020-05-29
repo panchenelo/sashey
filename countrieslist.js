44e6b66dd2f24d6ac54089fa222f81873577fc71
panchenelo <45730607+panchenelo@users.noreply.github.com>
2020-05-29 23:49:46 +0300

    Create countrieslist.js

---
+++ countrieslist.js
@@ -0,0 +1,51 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        nl: {
+            name: "Nederland",
+            code: "nl",
+            price: 49,
+            oldPrice: 98,
+            labelPrice: "EUR",
+            phoneHelper: "Bijvoorbeeld, +31 04338 5282",
+            nameHelper: "Jurrien Klumpjes",
+            nameError: "Naam (verplicht veld)",
+            phoneError: "Telefoonnummer invoeren",
+            countryError: "Land invoeren",
+            isDefault: true
+                },
+        
+        
+        
+        	be: {
+            name: "Belgien",
+            code: "be",
+            price: 49,
+            oldPrice: 98,
+            labelPrice: "EUR",
+            phoneHelper: "zum Beispiel, +32 123456789",
+            nameHelper: "Lucas Diallo",
+            nameError: "Der name erforderlich",
+            phoneError: "Numéro incorrectement spécifié",
+            countryError: "Nummer falsch angegeben"
+        },
+        
+        
+        
+        
+         lu: {
+            name: 'Lëtzebuerg ',
+            code: 'lu',
+            price: 49,
+            oldPrice: 98,
+            labelPrice: 'EUR',
+            phoneHelper: 'zum Beispill +352 12345678',
+            nameHelper: "Friedrich Müller",
+            nameError: "Der name erforderlich",
+            phoneError: "Numéro incorrectement spécifié",
+            countryError: "Nummer falsch angegeben"
+        }
+    };
+
+})();
